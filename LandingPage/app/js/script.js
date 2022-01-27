f5389d24c86cce190e7f5cd3a6ca596e9a431a1f
Chaitanya3315 <tkrishnachaitanya741@gmail.com>
2022-01-28 01:28:29 +0530

    hamburgerAnimation
    1.2

--- LandingPage/app/js/script.js
+++ LandingPage/app/js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const btnhamburger=document.querySelector('#btnhamburger');
+
+
+btnhamburger.addEventListener('click',function(){
+    if(btnhamburger.classList.contains('open')){
+        btnhamburger.classList.remove('open');
+    }else{
+        btnhamburger.classList.add('open');
+    }
+});
