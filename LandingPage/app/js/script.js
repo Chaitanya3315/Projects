eceffc20994f01bbcb071edcb348172bc2f85e92
Chaitanya3315 <tkrishnachaitanya741@gmail.com>
2022-01-28 22:28:21 +0530

    overlay
    1.3

--- LandingPage/app/js/script.js
+++ LandingPage/app/js/script.js
@@ -1,10 +1,15 @@
 const btnhamburger=document.querySelector('#btnhamburger');
-
+const header=document.querySelector('.header');
+const overlay=document.querySelector('.overlay');
 
 btnhamburger.addEventListener('click',function(){
-    if(btnhamburger.classList.contains('open')){
-        btnhamburger.classList.remove('open');
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }else{
-        btnhamburger.classList.add('open');
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
